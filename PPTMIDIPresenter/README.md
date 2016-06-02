7468293c0af7103c4f6da58a1a386faa015359f2
Dan Pries <danielmpries@gmail.com>
2016-06-02 16:31:01 -0400

    Add files via upload

---
+++ PPTMIDIPresenter/README.md
@@ -0,0 +1,12 @@
+# PowerPoint MIDI Presenter Add-on
+A simple PowerPoint Add-On to change slides based on MIDI CC events
+
+This is in an early prototype stage with MIDI polling and slide tagging but no event assignments.  
+
+The following has to be done to leave prototype:
+- [ ] Ribbon Bar Global Presentation Settings for MIDI channel and device assignment
+- [ ] Ribbon Bar Global Presentation Settings for Section and Slide navigation
+- [ ] Ribbon Bar and/or Action Pane for active slide MIDI settings 
+- [ ] Slide Tag Interogation
+- [ ] Song Part UI Tag Editor
+- [ ] Ribbon Bar Global setting to toggle the add-in events on or off so that new presentations are not automatically interrogated or modified
---
+++ PPTMIDIPresenter/SlideData/SlideConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace MIDIPresenter.SlideData {
+	
+	/// <summary>
+	/// Represents a specific slides configuration
+	/// </summary>
+	[DataContract]
+	internal class SlideConfiguration {
+
+		[DataMember(Name="song", IsRequired=false)]
+		public string song { get; set; }
+
+		[DataMember(Name = "part", IsRequired = false)]
+		public string part { get; set; }
+
+		[DataMember(Name = "order", IsRequired = false)]
+		public int order { get; set; }
+
+		[DataMember(Name = "ccNumber", IsRequired = false)]
+		public int ccNumber { get; set; }
+
+	}
+}
